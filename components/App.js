c93e0a2cfd3c35145666554a4c29b77442f752b0
Marcin Różański <rozanskimarcin2016@gmail.com>
2018-05-03 13:30:54 +0200

    Add

---
+++ components/App.js
@@ -0,0 +1,71 @@
+var GIPHY_PUB_KEY = 'dc6zaTOxFJmzC';
+var GIPHY_API_URL = 'https://api.giphy.com';
+
+App = React.createClass({
+	
+	getInitialState() {
+		return {
+			loading: false,
+			searchingText: '',
+			gif: {}
+		};
+	},
+
+	handleSearch: function(searchingText) {  // 1.
+		this.setState({
+			loading: true  // 2.
+		});
+		this.getGif(searchingText).then(gif => {  // 3.
+			this.setState({  // 4
+				loading: false,  // a
+				gif: gif,  // b
+				searchingText: searchingText  // c
+			});
+		});
+	},
+
+	getGif: function(searchingText) {  // 1.
+		return new Promise(function(resolve, reject) {
+			const url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
+			const xhr = new XMLHttpRequest();  // 3.
+			xhr.open('GET', url);
+			xhr.onload = function() {
+				if (xhr.status === 200) {
+					const data = JSON.parse(xhr.responseText).data; // 4.
+						const gif = {  // 5.
+							url: data.fixed_width_downsampled_url,
+							sourceUrl: data.url
+						};
+						resolve(gif);  // 6.
+				}
+				else {
+					reject(xhr.statusText);
+					console.log('Błąd');
+				}
+			};
+			xhr.send();
+		})
+	},
+
+	render: function() {
+
+		const styles = {
+			margin: '0 auto',
+			textAlign: 'center',
+			width: '90%'
+		};
+
+		return (
+			<div style={styles}>
+					<h1>Wyszukiwarka GIFow!</h1>
+					<p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+					<Search onSearch={this.handleSearch}/>
+				<Gif
+					loading={this.state.loading}
+					url={this.state.gif.url}
+					sourceUrl={this.state.gif.sourceUrl}
+				/>
+			</div>
+		);
+	}
+});
